64fdc173dcc4cec6db349a26aac7507192d56e28
Aman Sharma <amansharmadev@outlook.com>
2025-07-26 18:00:29 +0530

    Limit mail to 10

--- pages/api/mail/[inbox].js
+++ pages/api/mail/[inbox].js
@@ -3,22 +3,22 @@ import Mail from "../../../models/Mail";
 import Log from "../../../models/Log";
 
 export default async function handler(req, res) {
-	if (req.method !== "GET") {
-		return res.status(404).send();
-	}
-	await db();
+    if (req.method !== "GET") {
+        return res.status(404).send();
+    }
+    await db();
 
-	const mails = await Mail.find({ _inbox: req.query.inbox }).sort({
-		createdAt: -1,
-	});
+    const mails = await Mail.find({ _inbox: req.query.inbox })
+        .sort({ createdAt: -1 })
+        .limit(10);
 
-	res.send(mails);
+    res.send(mails);
 
-	/**
-	 * Logging which inboxs are viewed most
-	 */
-	Log.create({
-		to: req.query.inbox,
-		type: 'view',
-	});
+    /**
+     * Logging which inboxs are viewed most
+     */
+    Log.create({
+        to: req.query.inbox,
+        type: "view",
+    });
 }

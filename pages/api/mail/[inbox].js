959e9518f80fb182763f33c1b834e03c2f24a3d1
Aman Sharma <amansharmadev@outlook.com>
2026-03-07 00:33:50 +0530

    Fix: added cors for terminal app

--- pages/api/mail/[inbox].js
+++ pages/api/mail/[inbox].js
@@ -2,10 +2,17 @@ import db from "../../../utils/db";
 import Mail from "../../../models/Mail";
 import Log from "../../../models/Log";
 
+const headers = {
+    "Access-Control-Allow-Origin": "https://theamansharma.com",
+    "Access-Control-Allow-Headers": "Content-Type",
+};
+
 export default async function handler(req, res) {
     if (req.method !== "GET") {
         return res.status(404).send();
     }
+    if (event.httpMethod === "OPTIONS") return { statusCode: 200, headers };
+
     await db();
 
     const mails = await Mail.find({ _inbox: req.query.inbox })
